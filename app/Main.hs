57f5b945369129e230275b8cc8bc87468e07af06
Tanaka Kazunari <tzskp1@gmail.com>
2018-01-03 00:11:59 +0900

    tori2

--- app/Main.hs
+++ app/Main.hs
@@ -1,74 +1,77 @@
 import Text.Parsec
 import Data.Functor.Identity
+import Data.Char
 
-data JNum = Int | Float deriving Show
-data JStr = Char | JHex Char Char Char Char | String deriving Show
 data JValue =
-    JString JStr
-  | JNumber JNum
-  | JObject [(String,JValue)]
+    JString [[Char]]
+  | JNumber (Int,Int,Int)        -- int,frac,exp
+  | JObject [(JValue,JValue)]
   | JArray [JValue]
   | JTrue
   | JFalse
   | JNull
   deriving Show
 
-digits1_9 = foldl (<|>) (parserFail "") (map char "123456789")
-hex = foldl (<|>) (parserFail "") (map char "0123456789ABCDEFabcdef")
-
-fracPart :: ParsecT [Char] u Data.Functor.Identity.Identity [Char]
-fracPart = (:) <$> char '.' <*> many digit
+intOfStr [] = 0
+intOfStr xs = foldl (\a b -> 10 * a + b) 0 $ map digitToInt xs
+numOf [x,y,z] = JNumber (x,y,z)
 -- なぜか型注釈がないとコンパイルエラーになる
-expPart :: ParsecT [Char] u Data.Functor.Identity.Identity [Char]
-expPart = (:) <$> (char 'e' <|> char 'E') <*>
-          (((:) <$> char '+' <*> many digit) <|> 
-           ((:) <$> char '-' <*> many digit) <|>
-           many digit)
-intPart = sequence [char '0'] <|>
-          ((:) <$> digits1_9 <*> (many digit))
-plusPart = (++) <$> intPart <*>
-           ((++) <$> (fracPart <|> sequence [])
-           <*> (expPart <|> sequence []))
-minusPart = (:) <$> char '-' <*> plusPart
-numParser = spaces *> (minusPart <|> plusPart) <* spaces
-            
+fracPart :: ParsecT [Char] u Identity Int
+fracPart = ((char '.') *> (intOfStr <$> many digit))
+           <|> (return 0)
+expPart :: ParsecT [Char] u Identity Int
+expPart = ((char 'e' <|> char 'E')
+           *> (char '+' *> (intOfStr <$> many digit))
+           <|> (char '-' *> (((* (-1)) . intOfStr) <$> many digit))
+           <|> (intOfStr <$> many digit))
+          <|> (return 0)
+intPart :: ParsecT [Char] u Identity Int
+intPart = (char '-' *> (((* (-1)) . intOfStr)
+                        <$> ((:) <$> digit <*> many digit)))
+          <|> (intOfStr <$> ((:) <$> digit <*> many digit))
+numParser = spaces
+            *> (numOf <$> sequence [intPart,fracPart,expPart])
+            <* spaces
 
-strParenS = spaces *> sequence [char '"']
-strParenE = sequence [char '"'] <* spaces 
+hex :: ParsecT [Char] u Identity Char
+hex = foldl (<|>) (parserFail "") (map char "0123456789ABCDEFabcdef")
+strParenS = spaces *> char '"'
+strParenE = (char '"' <* spaces) *> (return [])
 hexPart = char 'u' *> sequence [hex,hex,hex,hex]
 escPart = char '\\' *> hexPart
 ctlPart = escPart
-  
-strPart = (++) <$>
-  (escPart <|> ctlPart <|> sequence [anyChar]) <*>
-  (strParenE <|> strPart)
+attach v (ys:xs) = (v:ys):xs
+attach v [] = [[v]]
+strPart = (([]:) <$> ((:) <$> (escPart <|> ctlPart)
+                      <*> (strParenE <|> strPart)))
+          <|> (attach <$> anyChar <*> (strParenE <|> strPart))
+strParser = JString <$> (try (strParenS *> strParenE)
+                          <|> (strParenS *> strPart))
 
-strParser = (try ((++) <$> strParenS <*> strParenE)) <|>
-            ((++) <$> strParenS <*> strPart)
+arrParenS = spaces *> char '['
+arrParenE = (char ']' <* spaces) *> (return [])
+arrPart = (:) <$> valParser <*>
+          (arrParenE <|> (char ',' *> arrPart))
+arrParser = JArray <$> (try (arrParenS *> arrParenE)
+                         <|> (arrParenS *> arrPart))
 
-arrParenS = spaces *> sequence [char '[']
-arrParenE = sequence [char ']'] <* spaces 
-arrPart = (++) <$> valParser <*>
-          (arrParenE <|> ((:) <$> char ',' <*> arrPart))
-arrParser = (try ((++) <$> arrParenS <*> arrParenE)) <|>
-            ((++) <$> arrParenS <*> arrPart)
+objParenS = spaces *> char '{'
+objParenE = (char '}' <* spaces) *> (return [])
+pair x y = (x,y)
+pairPart = pair <$> strParser <*> (char ':' *> valParser)
+objPart = (:) <$> pairPart <*>
+          (objParenE <|> (char ',' *> objPart))
+objParser = JObject <$> (try (objParenS *> objParenE)
+                          <|> (objParenS *> objPart))
 
-objParenS = spaces *> sequence [char '{']
-objParenE = sequence [char '}'] <* spaces 
-pairPart = (++) <$> strParser <*>
-          ((:) <$> char ':' <*> valParser)
-objPart = (++) <$> pairPart <*>
-          (objParenE <|>
-           ((:) <$> char ',' <*> objPart))
-objParser = (try ((++) <$> objParenS <*> objParenE)) <|>
-            ((++) <$> objParenS <*> objPart)
+boolParser = spaces
+             *> ((string "true" *> return JTrue) <|> (string "false" *> return JFalse))
+             <* spaces
 
-boolParser = string "true" <|> string "false"
-
-valParser = try strParser <|> try numParser <|>
-            try objParser <|> try arrParser <|>
-            boolParser <|> string "null"
--- jsonParser = (many space) >> (char '{')
+valParser = try strParser <|> try numParser
+            <|> try objParser <|> try arrParser
+            <|> try boolParser
+            <|> spaces *> (string "null" *> return JNull) <* spaces
 
 main = do
   js <- readFile "./test.json"
